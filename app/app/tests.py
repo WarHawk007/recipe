90ac660d19b68ffe473bc3b19451a65553f1a92c
farhansaeed07 <farhan.saeed@arbisoft.com>
2023-04-22 18:29:05 +0500

    Fix Linting errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,9 @@
-
 from django.test import SimpleTestCase
-
 from app.calc import add
 
+
 class CalcTestCase(SimpleTestCase):
-    
+
     def test_add_numbers(self):
         res = add(2, 3)
         self.assertEqual(res, 5)
